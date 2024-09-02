cf5736a67215ad03f307695819764bd806fb9ff4
Aakarshit07 <aakarshit070@gmail.com>
2024-09-02 22:43:42 +0530

    Build Settings Page and Form

---
+++ src/components/forms/workflow-form.tsx
@@ -0,0 +1,12 @@
+'use client'
+import React from 'react'
+
+type Props = {}
+
+const WorkflowForm = (props: Props) => {
+  return (
+    <div>WorkflowForm</div>
+  )
+}
+
+export default WorkflowForm
